b9e2a449d7ae459802512d9ed106dca0cdff56ff
Borja <borjajg02@gmail.com>
2023-11-20 16:24:14 +0100

    Parte del proyecto sobre los Custumer  (Client y Society)

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,28 @@
 public interface Customer {
+
+    public String getClientCode();
+
+    public void setClientCode(String ClientCode);
+
+    public String getFullName();
+
+    public String getId();
+
+    public String getPostalAddress();
+    public void setPostalAddress(String postalAddress);
+
+    public String getTown();
+    public void setTown(String townty);
+
+    public String getProvince();
+    public void setProvince(String province);
+
+    public String getEmail();
+    public void setEmail(String email);
+
+    public String getPhone();
+    public void setPhone(String phone);
+
+
+
 }
